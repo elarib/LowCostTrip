58c02c9c9f2cc631cf3b6ddae3cea45c0cd9b4c0
Abdelhamide EL ARIB <elarib.abdelhamide@gmail.com>
2016-11-20 15:16:41 +0100

    add Google Map

--- src/main/webapp/app/searchhotel/searchhotel.controller.js
+++ src/main/webapp/app/searchhotel/searchhotel.controller.js
@@ -9,25 +9,82 @@
 
     function SearchHotelController ($scope, Principal, LoginService, $state) {
         var vm = this;
+        initialize();
 
-        vm.account = null;
-        vm.isAuthenticated = null;
-        vm.login = LoginService.open;
-        vm.register = register;
-        $scope.$on('authenticationSuccess', function() {
-            getAccount();
-        });
+          function initialize() {
+              var map = new google.maps.Map(document.getElementById('map-canvas'), {
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+              });
+              var defaultBounds = new google.maps.LatLngBounds(
+                  new google.maps.LatLng(-33.8902, 151.1759),
+                  new google.maps.LatLng(-33.8474, 151.2631));
+              map.fitBounds(defaultBounds);
 
-        getAccount();
+              var input = /** @type {HTMLInputElement} */(document.getElementById('target'));
+              var searchBox = new google.maps.places.SearchBox(input);
+              var markers = [];
 
-        function getAccount() {
-            Principal.identity().then(function(account) {
-                vm.account = account;
-                vm.isAuthenticated = Principal.isAuthenticated;
-            });
-        }
-        function register () {
-            $state.go('register');
-        }
+              input.onkeyup = function(e) {
+                setTimeout(function() {
+                   var places = document.querySelectorAll('.pac-item');      
+                   for (var i=0;i<places.length;i++) {
+                      var place=places[i];
+                      var pac_icon = place.querySelector('.pac-icon').outerHTML
+                      var place_name_reduced = place.querySelector('.pac-item-query').outerHTML;
+                      place.innerHTML = pac_icon+place_name_reduced;
+                   }
+                }, 150);
+              }
+              
+              google.maps.event.addListener(searchBox, 'places_changed', function() {
+               input.value=input.value.split(',')[0]; 
+                var places = searchBox.getPlaces();
+                for (var i = 0, marker; marker = markers[i]; i++) {
+                  marker.setMap(null);
+                }
+                markers = [];
+                var bounds = new google.maps.LatLngBounds();
+                for (var i = 0, place; place = places[i]; i++) {
+                  var image = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                  };
+
+                  var marker = new google.maps.Marker({
+                    map: map,
+                    icon: image,
+                    title: place.name,
+                    position: place.geometry.location
+                  });
+
+                  markers.push(marker);
+
+                  bounds.extend(place.geometry.location);
+                }
+
+                map.fitBounds(bounds);
+              });
+
+              google.maps.event.addListener(map, 'bounds_changed', function() {
+                var bounds = map.getBounds();
+                searchBox.setBounds(bounds);
+              });
+                
+             var button = document.getElementById('button'); 
+              button.onclick = function() {
+                input.value='test';
+                input.focus(); 
+              }
+            }
+
+            google.maps.event.addDomListener(window, 'load', initialize);
+
+    
     }
+
+
+
 })();
