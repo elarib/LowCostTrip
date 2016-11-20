d49c555517a94929eae8b357afe17f8ba6b1ecf1
ys01se <ys01se@gmail.com>
2016-11-20 13:55:45 +0000

    maps

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -15,7 +15,6 @@
             'ui.bootstrap.datetimepicker',
             'ui.router',
             'infinite-scroll',
-            'checklist-model',
             'uiGmapgoogle-maps',
             // jhipster-needle-angularjs-add-module JHipster will add new module here
             'angular-loading-bar'
@@ -30,25 +29,29 @@
     }
 })();
 
-angular.module('lowCostTripApp').controller("checkCtrl",['$scope',function($scope){
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-    $scope.roles = [
-        {id: 1, text: 'guest'},
-        {id: 2, text: 'user'},
-        {id: 3, text: 'customer'},
-        {id: 4, text: 'admin'}
-    ];
-    $scope.user = {
-        roles: [2, 4]
-    };
-    $scope.checkAll = function() {
-        $scope.user.roles = $scope.roles.map(function(item) { return item.id; });
-    };
-    $scope.uncheckAll = function() {
-        $scope.user.roles = [];
-    };
-    $scope.checkFirst = function() {
-        $scope.user.roles.splice(0, $scope.user.roles.length);
-        $scope.user.roles.push(1);
-    };
+angular.module('lowCostTripApp').
+controller("checkCtrl",['$scope', '$log', 'uiGmapGoogleMapApi',
+    function ($scope, $log, GoogleMapApi){
+    angular.extend($scope, {
+        map: {center:
+        {
+            latitude: 40.1451,
+            longitude: -99.6680
+        },
+            zoom: 4
+        },
+        searchbox: {
+            template:'searchbox.tpl.html',
+            events:{
+                places_changed: function (searchBox) {}
+            }
+        },
+        options: {
+            scrollwheel: false
+        }
+    });
+
+    GoogleMapApi.then(function(maps) {
+        maps.visualRefresh = true;
+    });
 }]);
