7bf9a945791dacef28335052da2f671c8c623d15
Abdelhamide EL ARIB <elarib.abdelhamide@gmail.com>
2016-11-26 19:21:10 +0100

    new url

--- src/main/java/com/elarib/lowcosttrip/web/rest/SearchHotelResource.java
+++ src/main/java/com/elarib/lowcosttrip/web/rest/SearchHotelResource.java
@@ -17,6 +17,10 @@ import com.google.maps.model.PriceLevel;
 public class SearchHotelResource {
 	
 	public static String GOOGLE_MAPS_API_KEY = "AIzaSyAVyvH42WZzRKQK1Jr7Am_9S5vHyF42OaQ";
+	public static String AUTO_SUGGEST_URL = "http://secure.rezserver.com/api/autosuggest/?format=jsonp&product=hotel&refid=2050&query=[CITY]&cities=true&airports=true&hotels=false&regions=true&pois=false&plugin=&pet_friendly=false&path=hotel&jsoncallback=jQuery20309141397151075861_1480183644575&_=1480183644583";
+	public static String SEARCH_RESULT_API = "http://secure.rezserver.com/hotels/results/list?refid=7723&varid=1a2b3a4b73b74a75b&city_id=800029889&type=city&rooms=1&adults=1&children=0&check_in=12%2F13%2F2016&check_out=12%2F16%2F2016&date_search=1&currency=EUR&radius=0&distance_unit=mile&search_type=city";
+	public static String NEW_PREFIX_IMG= "max1024x768";
+	public static String OLD_PREFIX_IMG= "square150";
 	
 	@RequestMapping("/searchHotel")
 	public String test(HttpServletRequest req) throws Exception {
