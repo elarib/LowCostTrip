a3345cdc7b62625e742cade7da8e8dcd9b743084
Abdelhamide EL ARIB <elarib.abdelhamide@gmail.com>
2016-11-27 18:07:21 +0100

    save

--- src/main/webapp/app/searchhotel/searchhotel.controller.js
+++ src/main/webapp/app/searchhotel/searchhotel.controller.js
@@ -17,17 +17,22 @@
         var vm = this;
         initialize();
 
-
-        
-      searchHotel(800029889);
+        $scope.errorDisplay = true;
+        $scope.errors=[];
+        $scope.checkInDate=null;
+        $scope.checkInDate=null;
+        $scope.choosenCity=null;
+      //searchHotel(800029889);
 
         $scope.chooseCity = function (selected) {
            
             if(typeof selected !== "undefined"){
                console.log(selected); 
+               $scope.choosenCity = selected.originalObject;
                updateMap(selected.originalObject.coord.split(","))
 
-
+                updateVars();
+                console.log($scope.errors);
                 searchHotel(selected.originalObject.id);
 
 
@@ -37,7 +42,67 @@
 
         };
 
+        function updateVars(){
+           $scope.errors = [
+                    {
+                    'name':"Check In Date",
+                    'state' : $scope.checkInDate==null
+                    },
+                    {
+                    'name':"Check Out Date",
+                    'state' : $scope.checkOutDate==null
+                    },
+                    {
+                    'name':"City",
+                    'state' : $scope.choosenCity==null
+                    }
+                ];
+        }
+
+        $scope.checkInDate = null;
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+          };
+
+          $scope.$watch("checkInDate", function(v){
+            $scope.dateOptions2.minDate = new Date($scope.checkInDate);
+            updateVars();
+          });
+          $scope.$watch("checkOutDate", function(v){
+          
+            updateVars();
+          });
+
+          $scope.dateOptions2 = {
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date($scope.checkInDate),
+            startingDay: 1
+          };
+
+          $scope.altInputFormats = ['M!/d!/yyyy'];
+
+         $scope.open1 = function() {
+          $scope.popup1.opened = true;
+        };
+         $scope.open2 = function() {
+          $scope.popup2.opened = true;
+        };
+
+         $scope.popup1 = {
+            opened: false
+          };
+
+          $scope.popup2 = {
+            opened: false
+          };
+
+
         function searchHotel(cityId){
+          $scope.errorDisplay = false;
             $http({
               method: 'GET',
               url: "http://localhost:8080/api/searchHotel?cityID="+cityId
